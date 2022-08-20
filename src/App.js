5f7d59c9f7f9c3ded5c18a63ad3252651bed8a97
Justus Kiplimo <59371869+jk45limo@users.noreply.github.com>
2022-08-20 21:03:40 +0300

    using react-router-dom to create routes to pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import {Routes, Route} from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/home.component';
 
+const HatsPage =() =>(
+  <div>
+    <h1> HatsPage</h1>
+  </div>
+)
+
 function App() {
   return (
     <div>
-    <HomePage />
+      <Routes>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/shop/hats' component={HatsPage}/>
+      </Routes>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,14 +1,14 @@
 import React from "react";
+import {useMatch} from 'react-router-dom';
 
 import './menu-item.styles.scss'
 
-const MenuItem= ({title, imageUrl, size}) => (
-    <div 
-    style={{
+const MenuItem= ({title, imageUrl, size, history, linkUrl , match}) => (
+    <div className={`${size} menu-item`} onClick = {() => history.push(`${match.url}${linkUrl}`)}>
+        <div className="background-image"
+        style={{
         backgroundImage : `url(${imageUrl})`
-    }}
-    className={`${size} menu-item`}
-    >
+        }}/>
         <div className="content">
             <h1 className="title">{title.toUpperCase()}</h1>
             <span className="subtitle">SHOP NOW</span>
@@ -16,4 +16,4 @@ const MenuItem= ({title, imageUrl, size}) => (
     </div>
 )
 
-export default MenuItem;
+export default useMatch (MenuItem);
