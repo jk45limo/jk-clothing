855297b594b0322ce6c874e69ebab9f3b3085174
Justus Kiplimo <59371869+jk45limo@users.noreply.github.com>
2022-08-22 21:56:51 +0300

    added the header component and the logo

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,11 +4,16 @@ import {withRouter} from 'react-router-dom';
 import './menu-item.styles.scss'
 
 const MenuItem= ({title, imageUrl, size, history, linkUrl , match}) => (
-    <div className={`${size} menu-item`} onClick = {() => history.push(`${match.url}${linkUrl}`)}>
-        <div className="background-image"
-        style={{
-        backgroundImage : `url(${imageUrl})`
-        }}/>
+    <div 
+        className={`${size} menu-item`} 
+        onClick = {() => history.push(`${match.url}${linkUrl}`)}
+        >
+        <div 
+            className="background-image"
+            style={{
+            backgroundImage : `url(${imageUrl})`
+            }}
+        />
         <div className="content">
             <h1 className="title">{title.toUpperCase()}</h1>
             <span className="subtitle">SHOP NOW</span>
