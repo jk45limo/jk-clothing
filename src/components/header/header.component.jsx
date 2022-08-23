0520317e2bb4750e71afc9df81a3392ad050b18e
Justus Kiplimo <59371869+jk45limo@users.noreply.github.com>
2022-08-23 17:23:45 +0300

    added the sign in page and its components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,14 +7,14 @@ import './header.styles.scss';
 
 const Header =() => (
     <div className='header'>
-        <Link className='logo-container'>
+        <Link className='logo-container' to="/">
             <Logo className='logo'/>
         </Link>
         <div className='options'>
-            <Link className='option' to='/shop'>
+            <Link className='option' to="/shop">
                 SHOP
             </Link>
-            <Link className='option' to='/contact'>
+            <Link className='option' to="/contact">
                 CONTACT
             </Link>
 
